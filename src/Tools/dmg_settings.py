fe8606e2d1ba9e617f07c0b01538261da82985e1
PaddleStroke <pierrelouis.boyer@gmail.com>
2024-12-30 18:00:01 +0100

    Core: Branding

--- src/Tools/dmg_settings.py
+++ src/Tools/dmg_settings.py
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-files = [f"{containing_folder}/FreeCAD.app"]
+files = [f"{containing_folder}/AstoCAD.app"]
 symlinks = {"Applications": "/Applications"}
-badge_icon = f"{containing_folder}/FreeCAD.app/Contents/Resources/freecad.icns"
+badge_icon = f"{containing_folder}/AstoCAD.app/Contents/Resources/freecad.icns"
 window_rect = ((200, 200), (600, 400))
-icon_locations = {"FreeCAD.app": (180, 150), "Applications": (420, 150)}
+icon_locations = {"AstoCAD.app": (180, 150), "Applications": (420, 150)}
 size = "3g"
