fc1007917f3baf3c00547a866e9f7a08cb8511cf
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-01-06 17:19:19 +0100

    Core: Add a way to lock toolbars that are in menubar/titlebar

--- src/Gui/MainWindow.cpp
+++ src/Gui/MainWindow.cpp
@@ -1561,6 +1561,7 @@ void MainWindow::onToolBarMenuAboutToShow()
     menu->addSeparator();
 
     Application::Instance->commandManager().getCommandByName("Std_ToggleToolBarLock")->addTo(menu);
+    Application::Instance->commandManager().getCommandByName("Std_DlgToggleTitleToolBarLock")->addTo(menu);
 }
 
 void MainWindow::populateToolBarMenu(QMenu* menu)
