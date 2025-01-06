ad6d41309527ab08ec0c2cdc2f222bef77ed5683
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-01-06 11:14:16 +0100

    Sketcher: Customize menus.

--- src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -4822,6 +4822,7 @@ void ViewProviderSketch::generateContextMenu()
     int selectedPoints = 0;
     int selectedConstraints = 0;
     int selectedBsplines = 0;
+    int selectedHasInternalAlign = 0;
     int selectedBsplineKnots = 0;
     int selectedOrigin = 0;
     int selectedEndPoints = 0;
@@ -4852,6 +4853,11 @@ void ViewProviderSketch::generateContextMenu()
                         }
                         else if (geo->is<Part::GeomBSplineCurve>()) {
                             ++selectedBsplines;
+                            ++selectedHasInternalAlign;
+                        }
+                        else if (geo->is<Part::GeomEllipse>() || geo->is<Part::GeomArcOfEllipse>()
+                            || geo->is<Part::GeomArcOfHyperbola>() || geo->is<Part::GeomArcOfParabola>()) {
+                            ++selectedHasInternalAlign;
                         }
                         else {
                             ++selectedConics;
@@ -4898,7 +4904,8 @@ void ViewProviderSketch::generateContextMenu()
             && !onlyOrigin) {
             menu << "Sketcher_BSplineInsertKnot"
                  << "Sketcher_BSplineIncreaseDegree"
-                 << "Sketcher_BSplineDecreaseDegree";
+                 << "Sketcher_BSplineDecreaseDegree"
+                 << "Sketcher_CompBSplineShowHideGeometryInformation";
         }
         else if (selectedBsplineKnots > 0 && selectedBsplineKnots == selectedPoints
                  && selectedEdges == 0 && !onlyOrigin) {
@@ -4906,6 +4913,9 @@ void ViewProviderSketch::generateContextMenu()
                 menu << "Sketcher_BSplineIncreaseKnotMultiplicity"
                      << "Sketcher_BSplineDecreaseKnotMultiplicity";
             }
+        }
+        if (selectedHasInternalAlign > 0 && selectedHasInternalAlign == selectedEdges) {
+            menu << "Sketcher_RestoreInternalAlignmentGeometry";
         }
         if (selectedEdges >= 1 && selectedPoints == 0 && selectedBsplines == 0 && !onlyOrigin) {
             menu << "Sketcher_Dimension";
