1c76a324ab503e053de07fbd2064050e6d2880a5
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-01-06 11:14:38 +0100

    PartDesign: Customize menus

--- src/Mod/PartDesign/Gui/Command.cpp
+++ src/Mod/PartDesign/Gui/Command.cpp
@@ -2709,6 +2709,7 @@ public:
         setRememberLast(false);
 
         addCommand("PartDesign_NewSketch");
+        addCommand("Sketcher_ValidateSketch");
         addCommand("Sketcher_MapSketch");
         addCommand("Sketcher_EditSketch");
     }
