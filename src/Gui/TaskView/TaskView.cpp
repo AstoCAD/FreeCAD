c2b02bbec6de68e873a31b741a679057dde8b443
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-01-29 10:01:07 +0100

    Overlay: Automatic resize of taskview.

--- src/Gui/TaskView/TaskView.cpp
+++ src/Gui/TaskView/TaskView.cpp
@@ -438,6 +438,10 @@ void TaskView::adjustMinimumSizeHint()
 {
     QSize ms = minimumSizeHint();
     setMinimumWidth(ms.width());
+
+    resize(ms.width(), height());
+    OverlayManager::instance()->adjustSizeOfAreaContaining(this);
+    OverlayManager::instance()->refresh(nullptr, false, false);
 }
 
 QSize TaskView::minimumSizeHint() const
@@ -617,8 +621,6 @@ void TaskView::showDialog(TaskDialog *dlg)
     triggerMinimumSizeHint();
 
     Q_EMIT taskUpdate();
-
-    OverlayManager::instance()->refresh();
 }
 
 void TaskView::removeDialog()
