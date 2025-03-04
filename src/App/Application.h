47d54565a590ea16d5dc0f8b6b7df3bcd6728f94
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-03-04 17:04:15 +0100

    Restore branding preferences when Clear all.

--- src/App/Application.h
+++ src/App/Application.h
@@ -473,6 +473,8 @@ public:
     bool hasLinksTo(const DocumentObject *obj) const;
     //@}
 
+    static const char* getUserParameterTemplatePath();
+
     friend class App::Document;
 
 protected:
