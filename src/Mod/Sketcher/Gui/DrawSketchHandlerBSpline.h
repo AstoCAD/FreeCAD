efff5ce0b0519e76acba4e89425f74a3be009b7c
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-04-02 16:10:06 +0200

    Sketcher: Polyline rework.

--- src/Mod/Sketcher/Gui/DrawSketchHandlerBSpline.h
+++ src/Mod/Sketcher/Gui/DrawSketchHandlerBSpline.h
@@ -915,14 +915,14 @@ void DSHBSplineController::secondKeyShortcut()
 template<>
 void DSHBSplineController::thirdKeyShortcut()
 {
-    auto firstchecked = toolWidget->getCheckboxChecked(WCheckbox::FirstBox);
-    toolWidget->setCheckboxChecked(WCheckbox::FirstBox, !firstchecked);
+    handler->undoLastPoint();
 }
 
 template<>
 void DSHBSplineController::fourthKeyShortcut()
 {
-    handler->undoLastPoint();
+    auto firstchecked = toolWidget->getCheckboxChecked(WCheckbox::FirstBox);
+    toolWidget->setCheckboxChecked(WCheckbox::FirstBox, !firstchecked);
 }
 
 template<>
@@ -931,7 +931,7 @@ void DSHBSplineController::configureToolWidget()
     if (!init) {  // Code to be executed only upon initialisation
         toolWidget->setNoticeVisible(true);
         toolWidget->setNoticeText(
-            QApplication::translate("TaskSketcherTool_c1_bspline", "Press F to undo last point.")
+            QApplication::translate("TaskSketcherTool_c1_bspline", "Press R to undo last point.")
         );
 
         QStringList names = {
@@ -942,7 +942,7 @@ void DSHBSplineController::configureToolWidget()
 
         toolWidget->setCheckboxLabel(
             WCheckbox::FirstBox,
-            QApplication::translate("TaskSketcherTool_c1_bspline", "Periodic (R)")
+            QApplication::translate("TaskSketcherTool_c1_bspline", "Periodic (F)")
         );
         toolWidget->setCheckboxToolTip(
             WCheckbox::FirstBox,
