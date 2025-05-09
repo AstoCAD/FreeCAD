a0695db4e991d3102627989b063d1f5ff37870b7
paddle <pierrelouis.boyer@gmail.com>
2025-05-09 18:15:39 +0200

    TechDraw: New Page

--- src/Mod/TechDraw/App/DrawTemplate.h
+++ src/Mod/TechDraw/App/DrawTemplate.h
@@ -45,6 +45,7 @@ public:
     App::PropertyLength Width;
     App::PropertyLength Height;
     App::PropertyEnumeration Orientation;
+    App::PropertyEnumeration Language;
 
     App::PropertyMap EditableTexts;
 
