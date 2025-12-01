f590ea377f59fb7cc026a84106153510739f0bae
paddle <pierrelouis.boyer@gmail.com>
2025-12-01 15:10:35 +0100

    Sketcher: Distance constraints: scale label distance on creation.

--- src/Mod/Sketcher/Gui/ViewProviderSketch.h
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.h
@@ -764,6 +764,7 @@ protected:
     void slotUndoDocument(const Gui::Document&);
     void slotRedoDocument(const Gui::Document&);
     void slotSolverUpdate();
+    void slotConstraintAdded(Sketcher::Constraint* constraint);
     void forceUpdateData();
     //@}
 
@@ -971,6 +972,7 @@ private:
     boost::signals2::connection connectUndoDocument;
     boost::signals2::connection connectRedoDocument;
     boost::signals2::connection connectSolverUpdate;
+    boost::signals2::connection connectConstraintAdded;
 
     QMetaObject::Connection screenChangeConnection;
 
