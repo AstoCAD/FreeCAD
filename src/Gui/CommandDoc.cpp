1ee34d40f3655e9a25fee30a4504ea0115cea4fc
paddle <pierrelouis.boyer@gmail.com>
2026-03-04 10:51:20 +0100

    PR - placement not active when read only.

--- src/Gui/CommandDoc.cpp
+++ src/Gui/CommandDoc.cpp
@@ -1865,7 +1865,9 @@ bool StdCmdPlacement::isActive()
         nullptr,
         ResolveMode::FollowLink
     );
-    return !(sel.empty() || std::ranges::any_of(sel, [](auto obj) { return obj->isFreezed(); }));
+    return !(sel.empty() || std::ranges::any_of(sel, [](auto obj) {
+                 return obj->isFreezed() || obj->getPlacementProperty()->isReadOnly();
+             }));
 }
 
 //===========================================================================
@@ -1908,7 +1910,10 @@ bool StdCmdTransformManip::isActive()
         nullptr,
         ResolveMode::FollowLink
     );
-    return (sel.size() == 1 && !sel.front()->isFreezed());
+    return (
+        sel.size() == 1 && !sel.front()->isFreezed()
+        && !sel.front()->getPlacementProperty()->isReadOnly()
+    );
 }
 
 //===========================================================================
