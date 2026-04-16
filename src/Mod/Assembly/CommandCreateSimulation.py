c5a881159cd96af7668da82bcbafb559e436e8fa
paddle <pierrelouis.boyer@gmail.com>
2026-04-16 17:47:28 +0200

    PR - Assembly: Fix issues in Simulation tool #29428

--- src/Mod/Assembly/CommandCreateSimulation.py
+++ src/Mod/Assembly/CommandCreateSimulation.py
@@ -855,7 +855,7 @@ class TaskAssemblyCreateSimulation(QtCore.QObject):
         self.form.GlobalErrorToleranceSpinBox.setProperty(
             "rawValue", self.simFeaturePy.fGlobalErrorTolerance
         )
-        self.setFrameValue(0)
+
         self.form.FramesPerSecondSpinBox.setValue(self.simFeaturePy.jFramesPerSecond)
 
     def setSpinboxPrecision(self, spinbox, precision, unit=App.Units.TimeSpan):
