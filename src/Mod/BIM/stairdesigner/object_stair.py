9e5d59de97337d1425b0eaf9d1c36be8d429b314
paddle <pierrelouis.boyer@gmail.com>
2026-07-23 11:47:42 +0200

    BIM: Stair designer tool

---
+++ src/Mod/BIM/stairdesigner/object_stair.py
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+"""The assembled Stair document proxy."""
+
+import math
+
+import FreeCAD
+import Part
+
+from stairdesigner import geometry
+
+
+QT_TRANSLATE_NOOP = FreeCAD.Qt.QT_TRANSLATE_NOOP
+translate = FreeCAD.Qt.translate
+
+from .object_stair_base import StairBaseMixin
+
+from .object_steps import StairStepsMixin
+
+from .object_stringers import StairStringerMixin
+
+from .object_handrails import StairHandrailMixin
+
+class StairProxy(
+    StairStepsMixin,
+    StairStringerMixin,
+    StairHandrailMixin,
+    StairBaseMixin,
+):
+    """Parametric proxy for a complete Stair Designer stair."""
+
+    Type = "StairDesigner"
+
+    def __init__(self, obj):
+        self._updating = True
+        obj.Proxy = self
+        self.Object = obj
+        self.set_properties(obj)
+        self._updating = False
