d46dee346cb1dcd915c771ff53cdf202ff157eb7
paddle <pierrelouis.boyer@gmail.com>
2026-07-28 10:35:15 +0200

    TechDraw: Section view rework

--- src/Mod/TechDraw/Gui/QGIView.h
+++ src/Mod/TechDraw/Gui/QGIView.h
@@ -93,6 +93,7 @@ public:
     enum {Type = UserType::QGIView};
     int type() const override { return Type;}
     QRectF boundingRect() const override;
+    QRectF contentBoundingRect() const { return frameRect(); }
     void paint( QPainter *painter,
                         const QStyleOptionGraphicsItem *option,
                         QWidget *widget = nullptr ) override;
@@ -137,6 +138,7 @@ public:
     void snapPosition(QPointF& position);
     void snapSectionView(const TechDraw::DrawViewSection* sectionView,
                          QPointF& newPosition);
+    void applySectionPlacementConstraint();
     Base::Vector3d projItemPagePos(TechDraw::DrawViewPart* item);
     void alignTo(QGraphicsItem*, const QString &alignment);
 
@@ -188,6 +190,10 @@ public:
     bool isExporting() const;
 
     virtual void setMovableFlag();
+    void setFrameForcedVisible(bool visible);
+
+Q_SIGNALS:
+    void positionChanged();
 
 protected:
     QGIView* getQGIVByName(std::string name) const;
@@ -217,6 +223,7 @@ private:
     bool m_innerView;                                                  //View is inside another View
     bool m_multiselectActivated;
     bool snapping;
+    bool m_frameForcedVisible{false};
 
     QPen m_pen;
     QBrush m_brush;
